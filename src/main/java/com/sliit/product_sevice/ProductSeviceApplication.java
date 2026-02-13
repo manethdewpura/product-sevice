77fba6fb509cf9a61885479d085a58dc315139c5
manethdewpura <dewpuramaneth@gmail.com>
2026-02-14 01:24:20 +0530

    Initialize Spring Boot project with Maven wrapper and basic structure. Added .gitignore, .gitattributes, and configuration files. Created main application and test classes.

---
+++ src/main/java/com/sliit/product_sevice/ProductSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.product_sevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSeviceApplication.class, args);
+	}
+
+}
